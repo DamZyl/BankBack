792592c5d916b8eada8e2a364600a6bdee865f94
DamZyl <damianzylka@outlook.com>
2020-04-05 15:03:52 +0200

    Refactor db config

---
+++ Bank/Infrastructure/Database/Configurations/BankConfiguration.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using BankEntity = Bank.Models.Bank;
+
+namespace Bank.Infrastructure.Database.Configurations
+{
+    public class BankConfiguration : IEntityTypeConfiguration<BankEntity>
+    {
+        public void Configure(EntityTypeBuilder<BankEntity> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.OwnsOne(
+                    x => x.Address,
+                    address =>
+                    {
+                        address.Property(x => x.Street).HasColumnName("Street");
+                        address.Property(x => x.Number).HasColumnName("Number");
+                        address.Property(x => x.PostCode).HasColumnName("PostCode");
+                        address.Property(x => x.City).HasColumnName("City");
+                        address.Property(x => x.Country).HasColumnName("Country");
+                    }
+                );
+        }
+    }
+}
